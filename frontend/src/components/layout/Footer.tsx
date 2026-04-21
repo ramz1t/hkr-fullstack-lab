d162fdfbcd464deb8f9000697a47e76b9a11418c
Timur Ramazanov <75879061+ramz1t@users.noreply.github.com>
2026-04-21 18:52:51 +0200

    feat(front): added footer

---
+++ frontend/src/components/layout/Footer.tsx
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+export default function Footer() {
+    return (
+        <nav className="footer">
+            <p>
+                Made by{' '}
+                <a href="https://github.com/ramz1t" target="_blank">
+                    Ramz1
+                </a>
+            </p>
+        </nav>
+    )
+}
--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from './Navbar'
 import './Layout.css'
+import Footer from './Footer'
 
 export default function Layout() {
     return (
@@ -9,6 +10,7 @@ export default function Layout() {
             <main className="layout-main">
                 <Outlet />
             </main>
+            <Footer />
         </div>
     )
 }
