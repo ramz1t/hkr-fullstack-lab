9ed042e7a9f1b82a405f72a31c929d7eb25e930c
Timur Ramazanov <75879061+ramz1t@users.noreply.github.com>
2026-04-21 19:22:47 +0200

    feat(front): init app routing and layout, not found component

---
+++ frontend/src/components/layout/NotFound.tsx
@@ -0,0 +1,11 @@
+import './NotFound.css'
+
+export default function NotFound() {
+    return (
+        <div className="not-found">
+            <h1>404</h1>
+            <h2>Page not found</h2>
+            <a href="/">Return to home page</a>
+        </div>
+    )
+}
